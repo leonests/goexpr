f9251c294cd8667ed1975e1c46babd2a89d4fff7
Leon LiuYang <leon@zuru.tech>
2022-05-15 13:27:30 +0800

    add rune stream

---
+++ rune_stream.go
@@ -0,0 +1,33 @@
+package rulengine
+
+type runeStream struct {
+	runes []rune
+	pos   int
+	len   int
+}
+
+func newRuneStream(expr string) *runeStream {
+	var runes []rune
+	for _, r := range expr {
+		runes = append(runes, r)
+	}
+	return &runeStream{
+		runes: runes,
+		pos:   0,
+		len:   len(runes),
+	}
+}
+
+func (rs *runeStream) flowForward() rune {
+	char := rs.runes[rs.pos]
+	rs.pos += 1
+	return char
+}
+
+func (rs *runeStream) flowBackward(step int) {
+	rs.pos -= step
+}
+
+func (rs *runeStream) notEOF() bool {
+	return rs.pos < rs.len
+}
